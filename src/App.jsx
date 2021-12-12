18b2a960d0f42914621ab228843f8484aca494af
OshadhaShiro <shirokumara49@gmail.com>
2021-12-12 22:36:01 +0530

    Setup App

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import Header from './Components/Header.jsx';
+import CollectionCard from './Components/CollectionCard';
+import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import PunkList from './Components/PunkList';
+import Main from './Components/Main.jsx';
+
+function App() {
+	const [punkListData, setPunkListData] = useState([]);
+	const [selectedPunk, setSelectedPunk] = useState([0]);
+
+	useEffect(() => {
+		const getMyNtfs = async () => {
+			const openseaData = await axios.get(
+				'https://testnets-api.opensea.io/assets?asset_contract_address=0xa5f58a09c1c693d8ECC62D20eAD168206D1dBFDB&order_direction=asc',
+			);
+			console.log(openseaData.data.assets);
+			setPunkListData(openseaData.data.assets);
+		};
+
+		return getMyNtfs();
+	}, []);
+
+	return (
+		<div className="app">
+			<Header />
+			{punkListData.length > 0 && (
+				<>
+					<Main punkListData={punkListData} selectedPunk={selectedPunk} />
+					<PunkList
+						punkListData={punkListData}
+						setSelectedPunk={setSelectedPunk}
+					/>
+				</>
+			)}
+		</div>
+	);
+}
+
+export default App;
