01584c8ed884bce0575f0761f7e21ac8ef1297d6
OshadhaShiro <shirokumara49@gmail.com>
2021-12-13 07:17:00 +0530

    Setup Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,12 +8,12 @@ const Header = () => {
 	return (
 		<div className="header">
 			<div className="logoContainer">
-				<img src={punklogo} className="punkLogo" />
+				<img src={punklogo} className="punkLogo" alt="" />
 			</div>
 
 			<div className="searchBar">
 				<div className="searchIconContainer">
-					<img src={searchIcon} />
+					<img src={searchIcon} alt="" />
 				</div>
 				<input
 					type="text"
